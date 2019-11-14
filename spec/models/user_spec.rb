f5b217175c9517f88b465320a302c37577520c89
Abraham Keleta <keletaabraham@gmail.com>
2019-11-14 13:21:40 -0800

    added authentication token to users, as well as simple spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,4 +16,21 @@ RSpec.describe User, type: :model do
   it { should validate_uniqueness_of(:email).case_insensitive }
   it { should validate_confirmation_of(:password) }
   it { should allow_value('example@domain.com').for(:email) }
+
+  it { should respond_to(:auth_token) }
+  it { should validate_uniqueness_of(:auth_token) }
+
+  describe 'generate unique authentication token' do
+    it 'generates token' do 
+      allow(Devise).to receive(:friendly_token).and_return("auniquetoken")
+      @user.generate_authentication_token!
+      expect(@user.auth_token).to eql("auniquetoken")
+    end 
+
+    it 'should reject a duplicated auth token' do 
+      existing_user = FactoryGirl.create(:user, auth_token: 'auniquetoken123')
+      @user.generate_authentication_token!
+      expect(@user.auth_token).not_to eql(existing_user.auth_token)
+    end 
+  end 
 end
