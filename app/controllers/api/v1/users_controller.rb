09628b9304dd368707e821ed116b807ab91fc724
Abraham Keleta <keletaabraham@gmail.com>
2019-11-14 10:30:01 -0800

    allowing app to have api subdomain constraint

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,12 @@
-class Api::V1::UsersController < ApplicationController
-  respond_to :json
+  module Api
+    module V1
+      class UsersController < ApplicationController
+        respond_to :json
+  
+        def show 
+          respond_with User.find(params[:id])
+        end 
+      end
+    end
+  end
 
-  def show 
-    respond_with User.find(params[:id])
-  end 
-end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,10 @@
 Rails.application.configure do
+
+
+  ###### MY CONFIGURATION #######
+  config.action_dispatch.tld_length = 0
+
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
