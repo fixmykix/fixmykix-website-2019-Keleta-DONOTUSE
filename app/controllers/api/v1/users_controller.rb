3b33fb3e58dbadc87963a7ebeabfdaa22125cb5b
Abraham Keleta <keletaabraham@gmail.com>
2019-11-14 14:02:30 -0800

    added create, destroy methods to user session

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -8,26 +8,26 @@
         end 
 
         def create 
-          @user = User.new(user_params)
-          if @user.save 
-            render json: @user, status: 201, location: [:api, @user]
+          user = User.new(user_params)
+          if user.save 
+            render json: user, status: 201, location: [:api, user]
           else 
-            render json: { errors: @user.errors }, status: 422
+            render json: { errors: user.errors }, status: 422
           end
         end 
 
         def update 
-          @user = User.find(params[:id])
-          if @user.update(user_params)
-            render json: @user, status: 200, location: [:api, @user]
+          user = User.find(params[:id])
+          if user.update(user_params)
+            render json: user, status: 200, location: [:api, user]
           else 
-            render json: { errors: @user.errors }, status: 422
+            render json: { errors: user.errors }, status: 422
           end 
         end
 
         def destroy 
-          @user = User.find(params[:id])
-          if @user.destroy
+          user = User.find(params[:id])
+          if user.destroy
             head 204
           else 
             head 401
