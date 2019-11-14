74647fc4985579e17e6954b2bbf0f95fae6f0f16
Abraham Keleta <keletaabraham@gmail.com>
2019-11-13 23:39:04 -0800

    finished adding tests for user controller

---
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::UsersController, type: :controller do
+  
+  before(:each) { request.headers['accept'] = "application/vnd.fixmykix.v1" }
+  
+  describe 'get users #show action' do 
+    before(:each) do 
+      @user = FactoryGirl.create :user 
+      get :show, params: { id: @user.id }, format: :json
+    end 
+
+    it 'should return user info' do 
+      user_response = JSON.parse(response.body, symbolize_names: true)
+      expect(user_response[:email]).to eql(@user.email)
+    end 
+  end 
+end
