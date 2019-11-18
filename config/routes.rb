8e78dc765e2b773fc4f19fb6690d1fd6d57d0496
Abraham Keleta <keletaabraham@gmail.com>
2019-11-17 18:20:33 -0800

    added json serializer

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
     scope module: :v1, 
           constraints: ApiConstraints.new(version: 1, default: true) do 
       resources :users, :only => [:create, :show, :update, :destroy, :index] do 
-        resources :products, :only => [:create, :update, :destroy]
+        resources :products, :only => [:create, :update, :destroy, :index]
       end 
       resources :sessions, :only => [:create, :destroy]
       resources :products, :only => [:show, :index, :create]
