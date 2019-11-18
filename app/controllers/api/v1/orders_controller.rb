8a8f2847b1f9762fbb1ca137d5dca98c54ccb04c
Abraham Keleta <keletaabraham@gmail.com>
2019-11-17 21:15:28 -0800

    added the order show and serializer

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -10,4 +10,12 @@ class Api::V1::OrdersController < ApplicationController
     render json: current_user.orders.find(params[:id])
   end
 
+  def create 
+    @order = current_user.orders.build(params)
+    if @order.save
+      render json: @order, status: 201, location: [:api, current_user, @order]
+    else
+      render json: { errors: @order.errors }, status: 422
+    end 
+  end 
 end
